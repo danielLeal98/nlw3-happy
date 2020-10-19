c20ff5a6173c9d2c5bb7f96e62f56da0cee03140
Daniel Matheus <37132172+danielLeal98@users.noreply.github.com>
2020-10-19 19:16:58 -0300

    Criação das telas: Home e Maps.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,28 +1,9 @@
-import React from "react";
-import { FiArrowRight } from "react-icons/fi";
-import "./styles/global.css";
-import "./styles/pages/landing.css";
-import logoImg from "./images/Logo.svg";
+import React from 'react';
+import './styles/global.css';
+import Routes from './routes';
 
 function App() {
-  return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy"></img>
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-        <div className="location">
-          <strong>Belford Roxo</strong>
-          <span>Rio de Janeiro</span>
-        </div>
-        <a href="" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0,0,0, 0.6)" />
-        </a>
-      </div>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
